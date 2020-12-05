7053380529bd7b899db5596255b446ebe2d98861
kathleenyoung1 <>
2020-12-05 10:18:25 -0600

    final touches

--- modeling/base_model.py
+++ modeling/base_model.py
@@ -13,6 +13,7 @@ from sklearn.linear_model import LinearRegression
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
+import random
 
 # pre-covid data
 pre = pd.read_csv("pre.csv")
@@ -48,24 +49,34 @@ Xpost = post[['num_reviews', 'num_ratings', "avg_rating"]]
 Ypost = post['total_reviews']
 
 # test train split
-Xpost_train, Xpost_test, Ypost_train, Ypost_test = train_test_split(Xpost, Ypost, train_size=.75)
+Xpost_train, Xpost_test, Ypost_train, Ypost_test = train_test_split(Xpost, Ypost, train_size=.75, random_state = 14)
 
 # fit model
 reg_post = LinearRegression().fit(Xpost_train, Ypost_train)
 
 # MSE
-# MSE precovid model on precovid data
-mse_pre_pre = metrics.mean_squared_error(Ypre_test, reg_pre.predict(Xpre_test))
-print("MSE pre-covid model, pre-covid data: " + str(mse_pre_pre))
+# Train MSE precovid model on precovid data
+train_mse_pre_pre = metrics.mean_squared_error(Ypre_train, reg_pre.predict(Xpre_train))
+print("Training MSE pre-covid model, pre-covid data: " + str(train_mse_pre_pre))
 
-# MSE postcovid model on postcovid data
-mse_post_post = metrics.mean_squared_error(Ypost_test, reg_post.predict(Xpost_test))
-print("MSE post-covid model, post-covid data: " + str(mse_post_post))
+# Test MSE precovid model on precovid data
+test_mse_pre_pre = metrics.mean_squared_error(Ypre_test, reg_pre.predict(Xpre_test))
+print("Testing MSE pre-covid model, pre-covid data: " + str(test_mse_pre_pre))
 
 # MSE precovid model on postcovid data
 mse_pre_post = metrics.mean_squared_error(Ypost_test, reg_pre.predict(Xpost_test))
 print("MSE pre-covid model, post-covid data: " + str(mse_pre_post))
 
 
+# r^2
+# Train r^2 precovid model on precovid data
+r2_pre_pre_train = metrics.r2_score(Ypre_train, reg_pre.predict(Xpre_train))
+print("Training r^2 pre-covid model, pre-covid data: " + str(r2_pre_pre_train))
 
+# Test r^2 precovid model on precovid data
+r2_pre_pre = metrics.r2_score(Ypre_test, reg_pre.predict(Xpre_test))
+print("Testing r^2 pre-covid model, pre-covid data: " + str(r2_pre_pre))
 
+# r^2 precovid model on postcovid data
+r2_pre_post = metrics.r2_score(Ypost_test, reg_pre.predict(Xpost_test))
+print("r^2 pre-covid model, post-covid data: " + str(r2_pre_post))
